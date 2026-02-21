07d65b4c2fa1a3b2db5fa16c2291f2f099e11035
Badri Nagarajan <badrinrc@gmail.com>
2026-02-21 10:53:11 -0500

    Delete tennisiq directory

--- tennisiq/cv/ball/augment.py
+++
@@ -1,7 +0,0 @@
-from __future__ import annotations
-
-import numpy as np
-
-
-def identity_augment(images: np.ndarray, heatmap: np.ndarray):
-    return images, heatmap
--- tennisiq/cv/court/augment.py
+++
@@ -1,7 +0,0 @@
-from __future__ import annotations
-
-import numpy as np
-
-
-def identity_augment(image: np.ndarray, keypoints):
-    return image, keypoints
