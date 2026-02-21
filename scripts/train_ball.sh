3752848b12a58165ce4cca1471b800d17d6c865c
Badri Nagarajan <badrinrc@gmail.com>
2026-02-21 10:53:21 -0500

    Delete scripts directory

--- scripts/train_ball.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-python3 -m tennisiq.cv.ball.train \
-  --dataset-root data/datasets/balltracking \
-  --raw-data-dir data/datasets/balltracking/images \
-  --prepare-if-missing \
-  "$@"
--- scripts/train_court.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-python3 -m tennisiq.cv.court.train \
-  --data-root data/datasets/court_identification \
-  "$@"
